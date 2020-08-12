1a394ffe88ccd13e835d0f504ee9c7330658d0f9
Jen Jayme <jeniferjayme@gmail.com>
2020-08-11 22:18:12 -0700

    Updates

---
+++ precache-manifest.2fa1fccd4e16c9eedf1cf2b272d4b115.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20c9e218d28e15e84a621a88906c771b",
+    "url": "/clickamajiggy/index.html"
+  },
+  {
+    "revision": "9aa4cf581e6aad07b488",
+    "url": "/clickamajiggy/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "924fcc79626ee157fb49",
+    "url": "/clickamajiggy/static/css/main.41b76e75.chunk.css"
+  },
+  {
+    "revision": "9aa4cf581e6aad07b488",
+    "url": "/clickamajiggy/static/js/2.274bc985.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/clickamajiggy/static/js/2.274bc985.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "924fcc79626ee157fb49",
+    "url": "/clickamajiggy/static/js/main.1d5c036e.chunk.js"
+  },
+  {
+    "revision": "5fad29c878f2f8bbbcba",
+    "url": "/clickamajiggy/static/js/runtime-main.1af2ca98.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/clickamajiggy/static/media/logo.5d5d9eef.svg"
+  }
+]);
