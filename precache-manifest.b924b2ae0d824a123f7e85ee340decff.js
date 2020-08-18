fca8fe2b1b296a80d67aaa2f483363e5eef61eaa
Jen Jayme <jeniferjayme@gmail.com>
2020-08-18 00:46:54 -0700

    Updates

---
+++ precache-manifest.b924b2ae0d824a123f7e85ee340decff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5e03835f4134bbfe5587c4ef8be3ac1",
+    "url": "/clickamajiggy/index.html"
+  },
+  {
+    "revision": "a765d956c65b38335de7",
+    "url": "/clickamajiggy/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "b77e0bd7c96f01984ff1",
+    "url": "/clickamajiggy/static/css/main.ead8e2c8.chunk.css"
+  },
+  {
+    "revision": "a765d956c65b38335de7",
+    "url": "/clickamajiggy/static/js/2.d0576aad.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/clickamajiggy/static/js/2.d0576aad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b77e0bd7c96f01984ff1",
+    "url": "/clickamajiggy/static/js/main.0cd48cff.chunk.js"
+  },
+  {
+    "revision": "5fad29c878f2f8bbbcba",
+    "url": "/clickamajiggy/static/js/runtime-main.1af2ca98.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/clickamajiggy/precache-manifest.948a3cc590866996aec36553f7e7cb90.js"
+  "/clickamajiggy/precache-manifest.b924b2ae0d824a123f7e85ee340decff.js"
 );
 
 self.addEventListener('message', (event) => {
